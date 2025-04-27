58efa0b9a0dde1433164d4547b71ea72fefe76d1
datsiii <dasha.tsivina@gmail.com>
2025-04-27 03:52:31 +0300

    dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,16 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.dokka") version "1.8.20"
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("com.google.dagger.hilt.android") version "2.44"
 }
+
+tasks.dokkaHtml {
+    outputDirectory.set(buildDir.resolve("dokka"))
+}
+
 
 android {
     namespace = "com.example.serfplayer"
@@ -49,6 +58,43 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    //exoplayer
+    implementation(libs.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
+    implementation(libs.androidx.media3.common)
+
+    implementation (libs.ui)
+    implementation (libs.androidx.room.runtime)
+    implementation (libs.hilt.android)
+    implementation (libs.accompanist.systemuicontroller)
+    kapt (libs.hilt.compiler)
+
+    // Icons - Extended
+    implementation (libs.androidx.material.icons.extended)
+    // Navigation
+    implementation (libs.androidx.navigation.compose)
+    // Room database
+    implementation (libs.androidx.room.ktx)
+    kapt (libs.androidx.room.compiler)
+    // Dagger Hilt - hiltViewModel()
+    implementation (libs.androidx.hilt.navigation.compose)
+    // Permission
+    implementation (libs.accompanist.permissions)
+    // Other (System UI, ModalBottomSheet)
+    implementation (libs.accompanist.systemuicontroller)
+    implementation (libs.accompanist.navigation.material)
+    // Coil
+    implementation (libs.coil.kt.coil.compose)
+    // material3
+    implementation (libs.material3)
+    implementation (libs.androidx.material3.window.size.class1)
+    // Drag and move
+    implementation (libs.composereorderable.reorderable)
+    // Motion Layout
+    implementation (libs.androidx.constraintlayout.compose)
+    // System UI
+    implementation (libs.accompanist.systemuicontroller.v0170)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
